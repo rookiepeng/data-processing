f2776786f2fa6e78481deb74140bd052f8677de3
rookiepeng <rookie.dev@gmail.com>
2015-10-26 16:16:51 -0500

    automatic measurement for vector controller

---
+++ Python/auto_vector_controller_measurement.py
@@ -0,0 +1,188 @@
+#!/usr/bin/python
+
+####
+# Automated measurement script for vector controller
+# Python 3
+#
+# Instruments:
+#        1) Agilent 8722ES Vector Network Analyzer
+#        2) Keysight B2962A Power Source
+#
+# Required driver:
+#        1) National Instruments 488.2 driver
+#
+# Required Python Library:
+#        1) PyVISA
+#
+# Saved data file:
+#        1) "amp <timestamp>": Amplitude data in dB
+#                              V1[amp(f1),amp(f2),...,amp(fn)]
+#                              V2[amp(f1),amp(f2),...,amp(fn)]
+#                              ...
+#                              Vm[amp(f1),amp(f2),...,amp(fn)]
+#        2) "phs <timestamp>": Phase data in degree
+#                              V1[phs(f1),phs(f2),...,phs(fn)]
+#                              V2[phs(f1),phs(f2),...,phs(fn)]
+#                              ...
+#                              Vm[phs(f1),phs(f2),...,phs(fn)]
+#
+# Version 1
+#
+# By: Zhengyu Peng
+#     zhengyu.peng@ttu.edu
+#     Oct. 26, 2015
+####
+
+import datetime
+import time
+import visa
+
+voltInit = 770	#mV
+voltStep = 5	#mV
+voltStop = 1500	#mV
+
+now = now=datetime.datetime.now()
+timestamp = now.strftime("%Y%m%d %H%M%S")
+
+rm = visa.ResourceManager()
+res = rm.list_resources()
+for x in range(0, len(res)):
+	print("{}. ".format(x) + res[x] + ", ", end="")
+print("\n")
+
+vnaNumber = input('Select the VNA Number --> ')
+vna = rm.open_resource(res[int(vnaNumber)])
+print(vna.query("*IDN?"))
+
+powerNumber = input('Select the Power Supply Number --> ')
+power = rm.open_resource(res[int(powerNumber)])
+print(power.query("*IDN?"))
+
+power.write(":sour1:func:mode volt")
+power.write(":sour1:volt "+"0.77")
+power.write(":sens1:curr:prot 0.08")
+power.write(":outp1 on")
+
+power.write(":sour2:func:mode volt")
+power.write(":sour2:volt "+"0.77")
+power.write(":sens2:curr:prot 0.08")
+power.write(":outp2 on")
+
+vna.write("S21")
+vna.write("FORM4")
+vna.write("MARKBUCK688")
+
+
+for voltmm in range (voltInit, voltStop + voltStep, voltStep):
+	volt = voltmm / 1000
+	power.write(":sour1:volt " + str(volt))
+	print("Current Voltage = " + str(volt) + " V")
+
+	print("-> Change to Log Mag View")
+	vna.write("LOGM")
+	time.sleep(5)	# 5s
+	print("-> Read Log Mag Data")
+	vna.write("OUTPMARK1")
+	amp = vna.read_raw()
+	amp = amp.decode("utf-8")
+	markAmp = amp[1:14]
+
+	print("-> Change to Phase View")
+	vna.write("PHAS")
+	time.sleep(5)	# 5s
+	print("-> Read Phase Data\n")
+	vna.write("OUTPMARK1")
+	phs = vna.read_raw()
+	phs = phs.decode("utf-8")
+	markPhs = phs[1:14]
+
+	famp = open('phase1 ' + timestamp, 'a')
+	famp.write(str(voltmm) + " " + str(770) + " " + markAmp + " " + markPhs + "\n" )
+	famp.close()
+	
+for voltmm in range (voltInit, voltStop + voltStep, voltStep):
+	volt = voltmm / 1000
+	power.write(":sour2:volt " + str(volt))
+	print("Current Voltage = " + str(volt) + " V")
+
+	print("-> Change to Log Mag View")
+	vna.write("LOGM")
+	time.sleep(5)	# 5s
+	print("-> Read Log Mag Data")
+	vna.write("OUTPMARK1")
+	amp = vna.read_raw()
+	amp = amp.decode("utf-8")
+	markAmp = amp[1:14]
+
+	print("-> Change to Phase View")
+	vna.write("PHAS")
+	time.sleep(5)	# 5s
+	print("-> Read Phase Data\n")
+	vna.write("OUTPMARK1")
+	phs = vna.read_raw()
+	phs = phs.decode("utf-8")
+	markPhs = phs[1:14]
+
+	famp = open('phase2 ' + timestamp, 'a')
+	famp.write(str(1500) + " " +str(voltmm) + " " + markAmp + " " + markPhs + "\n" )
+	famp.close()
+
+for voltmm in range (voltStop, voltInit - voltStep, -voltStep):
+	volt = voltmm / 1000
+	power.write(":sour1:volt " + str(volt))
+	print("Current Voltage = " + str(volt) + " V")
+
+	print("-> Change to Log Mag View")
+	vna.write("LOGM")
+	time.sleep(5)	# 5s
+	print("-> Read Log Mag Data")
+	vna.write("OUTPMARK1")
+	amp = vna.read_raw()
+	amp = amp.decode("utf-8")
+	markAmp = amp[1:14]
+
+	print("-> Change to Phase View")
+	vna.write("PHAS")
+	time.sleep(5)	# 5s
+	print("-> Read Phase Data\n")
+	vna.write("OUTPMARK1")
+	phs = vna.read_raw()
+	phs = phs.decode("utf-8")
+	markPhs = phs[1:14]
+
+	famp = open('phase3 ' + timestamp, 'a')
+	famp.write(str(voltmm) + " " + str(1500) + " " + markAmp + " " + markPhs + "\n" )
+	famp.close()
+	
+for voltmm in range (voltStop, voltInit - voltStep, -voltStep):
+	volt = voltmm / 1000
+	power.write(":sour2:volt " + str(volt))
+	print("Current Voltage = " + str(volt) + " V")
+
+	print("-> Change to Log Mag View")
+	vna.write("LOGM")
+	time.sleep(5)	# 5s
+	print("-> Read Log Mag Data")
+	vna.write("OUTPMARK1")
+	amp = vna.read_raw()
+	amp = amp.decode("utf-8")
+	markAmp = amp[1:14]
+
+	print("-> Change to Phase View")
+	vna.write("PHAS")
+	time.sleep(5)	# 5s
+	print("-> Read Phase Data\n")
+	vna.write("OUTPMARK1")
+	phs = vna.read_raw()
+	phs = phs.decode("utf-8")
+	markPhs = phs[1:14]
+
+	famp = open('phase4 ' + timestamp, 'a')
+	famp.write(str(770) + " " + str(voltmm) + " " + markAmp + " " + markPhs + "\n" )
+	famp.close()
+
+power.write(":outp1 off")
+power.write(":outp2 off")
+vna.close()
+power.close()
+print("--Finish--")
