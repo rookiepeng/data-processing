1711b66b7cb92c52c0390f774598c9da95abb9d4
rookiepeng <rookie.dev@gmail.com>
2015-11-05 10:02:10 -0600

    Full voltage sweep script

--- Python/auto_vector_controller_full_sweep.py
+++ Python/auto_vector_controller_full_sweep.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 ####
-# Automated measurement script for vector controller
+# Automated measurement script for vector controller (Full voltage sweept)
 # Python 3
 #
 # Instruments:
@@ -15,10 +15,10 @@
 #        1) PyVISA
 #
 # Saved data file:
-#        1) "Region1 <timestamp>": VI(mV) VQ(mV) Amp(dB) Phase(Degree)
-#        1) "Region2 <timestamp>": VI(mV) VQ(mV) Amp(dB) Phase(Degree)
-#        1) "Region3 <timestamp>": VI(mV) VQ(mV) Amp(dB) Phase(Degree)
-#        1) "Region4 <timestamp>": VI(mV) VQ(mV) Amp(dB) Phase(Degree)
+#         "<Number>Region1 <timestamp>": VI(mV) VQ(mV) Amp(dB) Phase(Degree)
+#         "<Number>Region2 <timestamp>": VI(mV) VQ(mV) Amp(dB) Phase(Degree)
+#         "<Number>Region3 <timestamp>": VI(mV) VQ(mV) Amp(dB) Phase(Degree)
+#         "<Number>Region4 <timestamp>": VI(mV) VQ(mV) Amp(dB) Phase(Degree)
 #
 #            \   Region4  /
 #             \          /
@@ -39,16 +39,17 @@
 #
 # By: Zhengyu Peng
 #     zhengyu.peng@ttu.edu
-#     Oct. 26, 2015
+#     Nov. 05, 2015
 ####
 
 import datetime
 import time
 import visa
 
-voltInit = 770	#mV
-voltStep = 5	#mV
-voltStop = 1510	#mV
+voltInit = 770	# mV
+voltStep = 5	# mV
+voltStepAmp = 5 # mV
+voltStop = 1500 + voltStepAmp	#mV
 
 now = now=datetime.datetime.now()
 timestamp = now.strftime("%Y%m%d %H%M%S")
@@ -69,45 +70,45 @@ print(power.query("*IDN?"))
 
 power.write(":sour1:func:mode volt")
 power.write(":sour1:volt "+"0.77")
-power.write(":sens1:curr:prot 0.08")
+power.write(":sens1:curr:prot 0.2")
 power.write(":outp1 on")
 
 power.write(":sour2:func:mode volt")
 power.write(":sour2:volt "+"0.77")
-power.write(":sens2:curr:prot 0.08")
+power.write(":sens2:curr:prot 0.2")
 power.write(":outp2 on")
 
-vna.write("S21")
-vna.write("FORM4")
-vna.write("MARK1")
+vna.write("S21")	# Measure S21
+vna.write("FORM4")	# ASCII Format
+vna.write("MARK1")	# enable MARK 1
 vna.write("MARKBUCK688")  # set the Mark to 688th point, which is 24.3 GHz with the range from 20 GHz to 30 GHz and 1601 points
 
 count = 0;
 
-for voltInit in range (770, 1140, 10):
-	voltStop = voltStop - 10
+for voltInit in range (770, 1140, voltStepAmp):
+	voltStop = voltStop - voltStepAmp
 	count = count + 1
 	print("######## Voltage range: " + str(voltInit) + "V to " + str(voltStop) + "V ########\n")
 
 	print("### Region 1 ###")
 	for voltmm in range (voltInit, voltStop + voltStep, voltStep):
-		volt = voltmm / 1000
+		volt = voltmm / 1000	# Change to Volt
 		power.write(":sour1:volt " + str(volt))
-		print("Current Voltage = " + str(volt) + " V")
+		print("    Current Voltage = " + str(volt) + " V")
 
-		print("-> Change to Log Mag View")
+		print("    -> Change to Log Mag View")
 		vna.write("LOGM")
 		time.sleep(5)	# 5s
-		print("-> Read Log Mag Data")
+		print("        -> Read Log Mag Data")
 		vna.write("OUTPMARK1")
 		amp = vna.read_raw()
 		amp = amp.decode("utf-8")
 		markAmp = amp[1:14]
 
-		print("-> Change to Phase View")
+		print("    -> Change to Phase View")
 		vna.write("PHAS")
-		time.sleep(1)	# 5s
-		print("-> Read Phase Data\n")
+		time.sleep(1)	# 1s
+		print("        -> Read Phase Data")
 		vna.write("OUTPMARK1")
 		phs = vna.read_raw()
 		phs = phs.decode("utf-8")
@@ -121,21 +122,21 @@ for voltInit in range (770, 1140, 10):
 	for voltmm in range (voltInit, voltStop + voltStep, voltStep):
 		volt = voltmm / 1000
 		power.write(":sour2:volt " + str(volt))
-		print("Current Voltage = " + str(volt) + " V")
+		print("    Current Voltage = " + str(volt) + " V")
 
-		print("-> Change to Log Mag View")
+		print("    -> Change to Log Mag View")
 		vna.write("LOGM")
 		time.sleep(5)	# 5s
-		print("-> Read Log Mag Data")
+		print("        -> Read Log Mag Data")
 		vna.write("OUTPMARK1")
 		amp = vna.read_raw()
 		amp = amp.decode("utf-8")
 		markAmp = amp[1:14]
 
-		print("-> Change to Phase View")
+		print("    -> Change to Phase View")
 		vna.write("PHAS")
-		time.sleep(1)	# 5s
-		print("-> Read Phase Data\n")
+		time.sleep(1)	# 1s
+		print("        -> Read Phase Data")
 		vna.write("OUTPMARK1")
 		phs = vna.read_raw()
 		phs = phs.decode("utf-8")
@@ -149,21 +150,21 @@ for voltInit in range (770, 1140, 10):
 	for voltmm in range (voltStop, voltInit - voltStep, -voltStep):
 		volt = voltmm / 1000
 		power.write(":sour1:volt " + str(volt))
-		print("Current Voltage = " + str(volt) + " V")
+		print("    Current Voltage = " + str(volt) + " V")
 
-		print("-> Change to Log Mag View")
+		print("    -> Change to Log Mag View")
 		vna.write("LOGM")
 		time.sleep(5)	# 5s
-		print("-> Read Log Mag Data")
+		print("        -> Read Log Mag Data")
 		vna.write("OUTPMARK1")
 		amp = vna.read_raw()
 		amp = amp.decode("utf-8")
 		markAmp = amp[1:14]
 
-		print("-> Change to Phase View")
+		print("    -> Change to Phase View")
 		vna.write("PHAS")
-		time.sleep(1)	# 5s
-		print("-> Read Phase Data\n")
+		time.sleep(1)	# 1s
+		print("        -> Read Phase Data")
 		vna.write("OUTPMARK1")
 		phs = vna.read_raw()
 		phs = phs.decode("utf-8")
@@ -177,21 +178,21 @@ for voltInit in range (770, 1140, 10):
 	for voltmm in range (voltStop, voltInit - voltStep, -voltStep):
 		volt = voltmm / 1000
 		power.write(":sour2:volt " + str(volt))
-		print("Current Voltage = " + str(volt) + " V")
+		print("    Current Voltage = " + str(volt) + " V")
 
-		print("-> Change to Log Mag View")
+		print("    -> Change to Log Mag View")
 		vna.write("LOGM")
 		time.sleep(5)	# 5s
-		print("-> Read Log Mag Data")
+		print("        -> Read Log Mag Data")
 		vna.write("OUTPMARK1")
 		amp = vna.read_raw()
 		amp = amp.decode("utf-8")
 		markAmp = amp[1:14]
 
-		print("-> Change to Phase View")
+		print("    -> Change to Phase View")
 		vna.write("PHAS")
-		time.sleep(1)	# 5s
-		print("-> Read Phase Data\n")
+		time.sleep(1)	# 1s
+		print("        -> Read Phase Data")
 		vna.write("OUTPMARK1")
 		phs = vna.read_raw()
 		phs = phs.decode("utf-8")
